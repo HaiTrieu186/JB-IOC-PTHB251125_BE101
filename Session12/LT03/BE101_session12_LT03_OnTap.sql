bea5efa477ef69dafa7d2335d5088d2ee60d9287
HaiTrieu186 <pnhtrieu186@gmail.com>
2025-12-20 13:01:52 +0700

    Hoan thanh LT03 Session12

---
+++ Session12/LT03/BE101_session12_LT03_OnTap.sql
@@ -0,0 +1,80 @@
+create database session12;
+create schema if not exists lt3;
+
+set search_path to lt3;
+
+drop table products, sales;
+
+create table products
+(
+    product_id serial primary key,
+    name       varchar(50),
+    stock      int
+);
+
+create table sales
+(
+    sale_id    serial primary key,
+    product_id int references products (product_id),
+    quantity   int
+);
+
+insert into products(name, stock)
+values ('ABC', 100),
+       ('DEF', 50);
+
+--- KIEM TRA TRUOC INSERT
+create or replace function check_stock()
+    returns trigger
+as
+$$
+declare
+    v_stock int;
+begin
+    select stock
+    into v_stock
+    from products
+    where product_id = new.product_id;
+
+    if (new.quantity > v_stock) then
+        raise exception 'Khong du so luong hang trong kho !';
+    end if;
+
+    return new;
+end;
+$$ language plpgsql;
+
+create or replace trigger trg_check_Stock
+    before insert
+    on sales
+    for each row
+execute function check_stock();
+
+
+--- CAP NHAT SAU INSERT
+create or replace function update_stock()
+    returns trigger
+as
+$$
+declare
+begin
+    update products
+    set stock=stock - new.quantity
+    where product_id = new.product_id;
+    return new;
+end;
+$$ language plpgsql;
+
+create or replace trigger trg_update_Stock
+    after insert
+    on sales
+    for each row
+execute function update_stock();
+
+
+
+insert into sales(product_id, quantity)
+values (1, 30);
+
+insert into sales(product_id, quantity)
+values (1, 200);
